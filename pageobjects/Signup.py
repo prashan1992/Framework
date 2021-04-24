9dfd519ecb145a5797009b2f508994dd3a967259
prashan1992 <prashantkrdwivedi0709@gmail.com>
2021-04-24 18:57:29 +0530

    first commit

---
+++ pageobjects/Signup.py
@@ -0,0 +1,38 @@
+from selenium import webdriver
+
+class Signup:
+    textbox_firstname_id="first_name"
+    textbox_middlename_id="middle_name"
+    textbox_lastname_id="last_name"
+    textbox_emailid_id="email_id"
+    textbox_dob_id="dates2"
+    textbox_mobileno="dates3"
+    textbox_password_id="password"
+    button_submit_id="submit"
+
+    def __init__(self,driver):
+        self.driver=driver
+
+    def enter_first_name(self,firstname):
+        self.driver.find_element_by_id(self.textbox_firstname_id).send_keys(firstname)
+
+    def enter_middle_name(self,middlename):
+        self.driver.find_element_by_id(self.textbox_middlename_id).send_keys(middlename)
+
+    def enter_lastname(self,lastname):
+        self.driver.find_element_by_id(self.textbox_lastname_id).send_keys(lastname)
+
+    def enter_email_id(self,emailid):
+        self.driver.find_element_by_id(self.textbox_emailid_id).send_keys(emailid)
+
+    def enter_dob(self,dob):
+        self.driver.find_element_by_id(self.textbox_dob_id).send_keys(dob)
+
+    def enter_mobile_no(self,mobileno):
+        self.driver.find_element_by_id(self.textbox_mobileno).send_keys(mobileno)
+
+    def enter_pass_word(self,password):
+        self.driver.find_element_by_id(self.textbox_password_id).send_keys(password)
+
+    def submit(self):
+        self.driver.find_element_by_id(self.button_submit_id).click()
---
+++ test_tescases/signup.py
@@ -0,0 +1,8 @@
+import pytest
+from selenium import webdriver
+from pageobjects.Signup import Signup
+
+class Test_001_Signup:
+    def test_homepage(self):
+        self.driver=webdriver.Chrome(executable_path="C:/Users/91782/Desktop/Prashant/Driver/chromedriver.exe")
+        self.driver.get("https://maatribhasha.com/")
