9ca17adc5510d194b5096c7e7ce98db79c280d2f
prashan1992 <prashantkrdwivedi0709@gmail.com>
2021-04-24 19:00:09 +0530

    second commit

--- test_tescases/signup.py
+++ test_tescases/signup.py
@@ -6,3 +6,4 @@ class Test_001_Signup:
     def test_homepage(self):
         self.driver=webdriver.Chrome(executable_path="C:/Users/91782/Desktop/Prashant/Driver/chromedriver.exe")
         self.driver.get("https://maatribhasha.com/")
+        print("prashant")
