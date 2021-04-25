181a793f6390f180000c5efd2bc7b07690b2f75f
prashan1992 <prashantkrdwivedi0709@gmail.com>
2021-04-25 13:39:17 +0530

    1

--- test_tescases/test_testcases.py
+++ test_tescases/test_testcases.py
@@ -17,3 +17,6 @@ class Test_001_Signup(BaseClass):
         signup.enter_mobile_no("9451683489")
         signup.enter_pass_word("rudra@1234")
         signup.submit()
+
+    def test_login(self):
+        print(self)
