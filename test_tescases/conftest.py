da32f5565dcc982c9ed3654d4eb067eaec80fc0a
prashan1992 <prashantkrdwivedi0709@gmail.com>
2021-04-25 13:07:01 +0530

    third commit

--- test_tescases/conftest.py
+++ test_tescases/conftest.py
@@ -1,6 +1,11 @@
 from selenium import webdriver
 import pytest
+
+
 @pytest.fixture(scope="function")
-def setup():
+def setup(request):
     driver = webdriver.Chrome(executable_path="C:/Users/91782/Desktop/Prashant/Driver/chromedriver.exe")
     driver.get("https://maatribhasha.com/")
+    request.cls.driver = driver
+    yield
+    driver.quit()
